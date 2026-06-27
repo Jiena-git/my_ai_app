5298af5d2a897db8c0170460e95b7b8e76e000b7
Jiena-git <2291863963@qq.com>
2026-06-27 16:34:44 +0800

    大屏测试完成

--- ai/detect.py
+++ ai/detect.py
@@ -7,13 +7,19 @@ def detect_people(image_path):
 
     img = cv2.imread(image_path)
 
-    results = model(img)
+    results = model(img, imgsz=1280,conf=0.25, iou=0.5) #results = model(img)
 
     count = 0
 
     for r in results:
+        if r.boxes is None:
+            continue
+
         for box in r.boxes:
-            if int(box.cls[0]) == 0:
+            cls = int(box.cls[0])
+            conf = float(box.conf[0])
+
+            if cls == 0 and conf > 0.25:
                 count += 1
 
     return count
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
+import time
 from fastapi import FastAPI
 from ai.detect import detect_people
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()   #必须先创建 app
-# 再加 middleware
+app = FastAPI()
+
+last_count = 0
+last_image = None
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,9 +17,23 @@ app.add_middleware(
 )
 
 @app.get("/detect")
-def detect():
-    count = detect_people("data/images/PartA_00000.jpg")
+def detect(image_name: str):
+    start = time.time()
+
+    path = f"data/images/{image_name}"
+
+    try:
+        count = detect_people(path)
+    except Exception as e:
+        print("error:", e)
+        count = 0
+
+    if count is None:
+        count = 0
+
+    print("inference time:", time.time() - start)
 
     return {
-        "person_count": count
+        "person_count": int(count),
+        "image": image_name
     }
