a733e1308da6b3194b00eda67efae4761541567c
Jiena-git <2291863963@qq.com>
2026-06-27 14:36:31 +0800

    v1: 人数检测FastAPI基础测试完成

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from ai.detect import detect_people
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI()   #必须先创建 app
+# 再加 middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/detect")
 def detect():
